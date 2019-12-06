5f534dae3939084d3e735a818b95f70d8bf0b8f5
Rahul Shah <rahulshah7@outlook.com>
2019-12-06 14:50:24 +0000

    Implement requirements 1 to 4

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,7 +1,40 @@
 "use strict";
 
 describe("Thermostat", function() {
-  it("does nothing", function() {
-    expect().nothing();
+  let thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  describe("#temperature", function() {
+    it("starts at 20 degrees", function() {
+      expect(thermostat.temperature).toEqual(thermostat._DEFAULT_TEMPERATURE);
+    });
+  });
+
+  describe("#up", function() {
+    it("increases temperature by 1 degree", function() {
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(
+        thermostat._DEFAULT_TEMPERATURE + 1
+      );
+    });
+  });
+
+  describe("#down", function() {
+    it("decreases temperature by 1 degree", function() {
+      thermostat.down();
+      expect(thermostat.temperature).toEqual(
+        thermostat._DEFAULT_TEMPERATURE - 1
+      );
+    });
+
+    it("does not decrease the temperature to below 10 degrees", function() {
+      for (let i = 0; i < 15; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.temperature).toEqual(thermostat._MINIMUM_TEMPERATURE);
+    });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,20 @@
 "use strict";
 
-class Thermostat {}
+class Thermostat {
+  constructor() {
+    this._DEFAULT_TEMPERATURE = 20;
+    this._MINIMUM_TEMPERATURE = 10;
+    this._temperature = this._DEFAULT_TEMPERATURE;
+  }
+
+  get temperature() {
+    return this._temperature;
+  }
+
+  up() {
+    this._temperature++;
+  }
+  down() {
+    if (this._temperature > this._MINIMUM_TEMPERATURE) this._temperature--;
+  }
+}
